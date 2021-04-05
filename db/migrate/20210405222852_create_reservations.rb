d6aae5568e44eabc9c5ac2b1c9c01453cab23248
Walter Aab <aabwalter@yahoo.com>
2021-04-05 17:39:27 -0500

    Add 'deposit' and 'status' parameters to reservation migration

--- db/migrate/20210405222852_create_reservations.rb
+++ db/migrate/20210405222852_create_reservations.rb
@@ -3,6 +3,8 @@ class CreateReservations < ActiveRecord::Migration[6.1]
     create_table :reservations do |t|
       t.date :check_in
       t.date :check_out
+      t.decimal :deposit, :precision => 8, :scale => 2
+      t.string :status, default: "pending"
       t.integer :listing_id
       t.integer :borrower_id
 
