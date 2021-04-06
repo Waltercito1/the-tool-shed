db401b61a6a64761c48b7b867e877c41e99f246b
Walter Aab <aabwalter@yahoo.com>
2021-04-05 22:35:12 -0500

    Fix routes for users login and logout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,7 @@ module ApplicationHelper
     end
 
     def redirect_if_logged_in
-        redirect_to listings_path if logged_in?
+        redirect_to user_path(session[:user_id]) if logged_in?
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/show'
-  get 'signup' => 'users#new'
+  get '/users/new'
+  get '/users/create'
+  get '/users/show'
+  get '/signup', to: 'users#new'
   post '/users', to: 'users#create'
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   get '/logout', to: "sessions#destroy"
 
-  get '/' => 'sessions#new'
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
+  get '/', to: 'sessions#new'
+  #get '/login', to: 'sessions#new'
 
   resources :reviews
   resources :reservations
