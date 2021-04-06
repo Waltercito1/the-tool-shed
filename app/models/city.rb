81b2bc2a35e736607f9e4a6ab107dbd6ddcd1814
Walter Aab <aabwalter@yahoo.com>
2021-04-05 20:14:52 -0500

    Add sessions controller and '/' route

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,5 @@
 class City < ApplicationRecord
-    as_many :listings
+    has_many :listings
     has_many :reservations, :through => :listings
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+  get '/' => "sessions#welcome"  
   resources :reviews
   resources :reservations
   resources :cities
