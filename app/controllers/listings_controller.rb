d9382895ee684649ffcb561fa6661838386a5ea8
Walter Aab <aabwalter@yahoo.com>
2021-04-06 21:11:28 -0500

    Add CRUD funtionality to listings_controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,51 @@
 class ListingsController < ApplicationController
+    before_action :find_listing, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @listings = Listing.all
+    end
+
+    def show
+    end
+
+    def new 
+        @listing = Listing.new
+    end
+
+    def create
+        @listing = Listing.create(listing_params(:title, :category, :description, :price))
+        if @listing.valid?
+            redirect_to @listing
+        else
+            render :new
+        end
+    end
+
+    def update
+        @listing.update(listing_params(:title, :category, :description, :price))
+        if @listing.valid?
+            redirect_to @listing
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        if @listing.destroy
+            redirect_to listings_path
+        else
+            redirect_to @listing
+        end
+    end
+
+    private
+
+    def listing_params(*args)
+        params.require(:listing).permit(*args)
+    end
+
+    def find_listing
+        @listing = Listing.find_by_id(params[:id])
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   resources :reviews
   resources :reservations
   resources :cities
-  #resources :listings
+  resources :listings
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
