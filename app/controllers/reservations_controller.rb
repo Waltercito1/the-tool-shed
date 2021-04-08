24a4149269e602f21d2b43c35edb3b632ab194b4
Walter Aab <aabwalter@yahoo.com>
2021-04-08 11:21:42 -0500

    Add create action to reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,3 +1,32 @@
 class ReservationsController < ApplicationController
+    before_action :find_reservation, only: [:show, :edit, :update, :destroy]
     
+    def new 
+        @reservation = Reservation.new
+    end
+
+    def create
+        @reservation = Reservation.create(reservation_params)
+        #current_user.reservations.create(reservation_params)
+        if @reservation.valid? #current_user.reservations.last.valid?
+            redirect_to @reservation #current_user.reservations.last 
+        else
+            render :new
+        end
+    end
+
+    def show
+        find_reservation 
+    end
+
+    private
+
+    def reservation_params
+        params.require(:reservation).permit(:check_in, :check_out)
+    end
+
+    def find_reservation
+        @reservation = Reservation.find_by_id(params[:id])
+    end
+
 end
