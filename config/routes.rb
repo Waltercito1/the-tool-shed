dba53f03d04bef3f5cf84387ad49e00da2e9d9e4
Walter Aab <aabwalter@yahoo.com>
2021-04-08 09:50:25 -0500

    Add edit route, view, and action to user

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
   resources :reviews
   resources :reservations
   resources :listings
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
