3d1077c12c1fd30fa0452f36c92f9664b43c59a0
Walter Aab <aabwalter@yahoo.com>
2021-04-07 19:33:20 -0500

    Remove city helper, view folder, routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
 
   resources :reviews
   resources :reservations
-  resources :cities
   resources :listings
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
