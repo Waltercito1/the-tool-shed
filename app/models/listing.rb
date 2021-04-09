7af65bf45979c225a191b55090438546827ec5e0
Walter Aab <aabwalter@yahoo.com>
2021-04-08 21:29:05 -0500

    Work in progress on displaying errors for new and edit listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,7 @@ class Listing < ApplicationRecord
     has_many :reviews, :through => :reservations
     has_many :borrowers, :class_name => "User", :through => :reservations
 
-    validates :title,  presence: true
+    validates :title,  presence: true #, length: {minimum: 8}
     validates :category, presence: true
     validates :price, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
     has_many :reservations, :through => :listings
     has_many :booked_reservations, :class_name => "Reservation", :foreign_key => "borrower_id"
     has_many :accepted_reservations, :class_name => "Reservation", :foreign_key => "lender_id"
+    
     has_many :borrowed_tools, :foreign_key => "borrower_id", :class_name => "Reservation"
     has_many :reviews, :foreign_key => "borrower_id"
 
