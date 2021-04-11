72be15cd66f30a5214f8c2bce2247c609efec324
Walter Aab <aabwalter@yahoo.com>
2021-04-10 19:17:14 -0500

    Add password_confirmation to sign-up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :email, :password, :street, :city, :zipcode, :contact_info)
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :street, :city, :state, :zipcode, :contact_info)
     end
 
     def find_user
