ea7e53c18a90eb13d5ffc24b05a1565185e59bb2
Walter Aab <aabwalter@yahoo.com>
2021-04-21 00:41:26 -0500

    wip create a review for a reservation

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,24 @@
 class ReviewsController < ApplicationController
+
+    def new
+
+    end
+
+    def show
+
+    end
+
+    def index
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    
 end
